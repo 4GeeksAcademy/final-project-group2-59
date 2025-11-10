316b4f6059cf54049449a933f01524993231dd69
Eossandon92 <81490666+Eossandon92@users.noreply.github.com>
2025-11-10 17:16:11 +0000

    Front

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -12,6 +12,9 @@ export const Navbar = () => {
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link>
+					<Link to="/donations">
+						<button className="btn btn-primary m-1">donations</button>
+					</Link>
 				</div>
 			</div>
 		</nav>
