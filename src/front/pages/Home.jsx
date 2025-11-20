ab516d1b44eb87fe344d5d11079658c21ef1577d
juanpa-27181 <74931576+juanpa-27181@users.noreply.github.com>
2025-11-20 03:23:43 +0000

    repair backimg

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from "react"
+import React from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import "../styles/pages/style.css";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import "../styles/pages/stylle.css"
+import fondoAbout from "./img/fondo.jpeg";
+
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-	const { store, dispatch } = useGlobalReducer()
-
-	return (
-		<section className="hero">
-			<div className="hero-text">
-			<h2>Tu Refugio de Confianza:<br/> Cuidando Vidas, Buscando Hogares</h2>
-			<div className="buttons">
-				<button className="btn info">Más Información</button>
-				<button className="btn adopt">Adopta Ahora</button>
-			</div>
-			</div>
-		</section>
-	);
-}; 
+  return (
+    <section className="hero home-page">
+      <div className="hero-text">
+        <h2>Tu Refugio de Confianza:<br /> Cuidando Vidas, Buscando Hogares</h2>
+        <div className="buttons">
+          <button className="btn info">Más Información</button>
+          <button className="btn adopt">Adopta Ahora</button>
+        </div>
+      </div>
+    </section>
+  );
+};
