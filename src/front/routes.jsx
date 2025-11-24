22d108dc121455f3cf3ab975faa1768338c98b41
juanpa-27181 <74931576+juanpa-27181@users.noreply.github.com>
2025-11-24 05:03:21 +0000

    add new footer

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -6,11 +6,12 @@ import {
   Route,
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
-import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Donations } from "./pages/Donations"
 import { Register } from "./pages/Register";
+import { HomeAndAbout } from "./pages/Home";
+import { Footer } from "./pages/Footer";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,13 +25,13 @@ export const router = createBrowserRouter(
     <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
       {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomeAndAbout />} />
       <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
       <Route path="/demo" element={<Demo />} />
       <Route path="/donations" element={<Donations />} />
       <Route path="/register" element={<Register />} />
-
+      <Route path="/home" element={<HomeAndAbout />} />
+      <Route path="/" element={<Footer />} />
     </Route>
   )
-
 );
