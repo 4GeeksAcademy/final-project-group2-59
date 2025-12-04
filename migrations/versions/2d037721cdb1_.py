87b44d06eb625a40b821955c2c8470c954ffcc8a
Verónica Rodríguez <veronikbw@gmail.com>
2025-12-04 00:11:54 +0000

    Se revisan migraciones

--- migrations/versions/2d037721cdb1_.py
+++ migrations/versions/2d037721cdb1_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 18d6cf3f747a
+Revision ID: 2d037721cdb1
 Revises: 
-Create Date: 2025-12-03 22:46:12.296433
+Create Date: 2025-12-04 00:10:04.822335
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '18d6cf3f747a'
+revision = '2d037721cdb1'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -32,10 +32,10 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('birthdate', sa.DateTime(timezone=True), nullable=False),
-    sa.Column('species', sa.Enum('Perro', 'Gato', 'Otro', name='speciespetenum'), nullable=False),
+    sa.Column('species', sa.Enum('DOG', 'CAT', 'OTHER', name='speciespetenum'), nullable=False),
     sa.Column('breed', sa.String(length=100), nullable=True),
-    sa.Column('sex', sa.Enum('Macho', 'Hembra', name='sexpetenum'), nullable=False),
-    sa.Column('status', sa.Enum('Adoptado', 'Buscando Familia', name='statuspetenum'), nullable=False),
+    sa.Column('sex', sa.Enum('MALE', 'FEMALE', name='sexpetenum'), nullable=False),
+    sa.Column('status', sa.Enum('ADOPTED', 'LOOKING_FOR_FAMILY', name='statuspetenum'), nullable=False),
     sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
     sa.Column('description', sa.String(length=500), nullable=True),
     sa.Column('image', sa.String(length=255), nullable=True),
