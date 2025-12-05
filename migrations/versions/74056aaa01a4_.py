efe8141953bcaf4737faa44203eea871f2f450e2
Verónica Rodríguez <veronikbw@gmail.com>
2025-12-05 23:44:15 +0000

    Se agregan endpoints de admin de usuario

--- migrations/versions/74056aaa01a4_.py
+++ migrations/versions/74056aaa01a4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 853aecffac12
+Revision ID: 74056aaa01a4
 Revises: 
-Create Date: 2025-12-05 22:29:18.922012
+Create Date: 2025-12-05 23:42:50.189817
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '853aecffac12'
+revision = '74056aaa01a4'
 down_revision = None
 branch_labels = None
 depends_on = None
